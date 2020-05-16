770cda1a78bc76b9e2fcb602e6fe5d3af9126e3d
Jorgemayor <mayor.jorge@correounivalle.edu.co>
2020-05-16 12:16:07 -0500

    Implemented structure of input reading

--- Soluciones/solucion1.cpp
+++ Soluciones/solucion1.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <string.h>
+#include <utility>
 using namespace std;
 
-int main() {
+int n, m, k;
 
-  return 0;
+int getInput() {
+
+    n = 1;
+    k = 0;
+    m = 0;
+    pair<string,int> animales[n];
+    animales[0] = make_pair("cabra", 1); 	
+    cout<<"animal: " << animales[0].first << " grandeza: " << animales[0].second << endl;
+    
+    return 1;
+}
+
+int main() {
+    
+    getInput();
+    return 0;
 }
