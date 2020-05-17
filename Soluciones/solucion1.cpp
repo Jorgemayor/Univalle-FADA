87137979361631dbd01520ca75fbb43e7d445244
Jorgemayor <mayor.jorge@correounivalle.edu.co>
2020-05-17 11:34:56 -0500

    Chaged spaces for tabs

--- Soluciones/solucion1.cpp
+++ Soluciones/solucion1.cpp
@@ -9,22 +9,27 @@ using namespace std;
 int n, m, k;
 
 pair<string, int>* getInput(string testcase) {
+	
+	ifstream archivo("../Pruebas/"+testcase+".txt");
+	string name;
+	int grandeza;
+	archivo >> n >> m >> k;
+	pair<string,int>* animals = new pair<string, int>[n];
+	for(int i = 0; i<=n-1; i+=1){
+			archivo >> name >> grandeza;
+		animals[i] = make_pair(name, grandeza);
+	}
+	return animals;
+}
 
-    ifstream archivo("../Pruebas/"+testcase+".txt");
-    string name;
-    int grandeza;
-    archivo >> n >> m >> k;
-    pair<string,int>* animals = new pair<string, int>[n];
-    for(int i = 0; i<=n-1; i+=1){
-        archivo >> name >> grandeza;
-        animals[i] = make_pair(name, grandeza);
-    }
-    return animals;
+void someKindOfSort () {
+	
+
 }
 
 int main() {
-    
-    string testcase = "prueba1";
-    pair<string, int>* animals = getInput(testcase);
-    return 0;
+	
+	string testcase = "prueba1";
+	pair<string, int>* animals = getInput(testcase);
+	return 0;
 }
