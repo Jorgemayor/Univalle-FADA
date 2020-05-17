b2467c7237899490bc56ad1955b8f12315c65f19
Rhazek <cesar.becerra@correounivalle.edu.co>
2020-05-16 21:24:27 -0500

    Implemented reading of files

--- Soluciones/solucion1.cpp
+++ Soluciones/solucion1.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
 #include <string.h>
 #include <utility>
+#include <stdlib.h>
+#include <string.h>
+#include <fstream>
 using namespace std;
 
 int n, m, k;
 
 int getInput() {
 
-    n = 1;
-    k = 0;
-    m = 0;
+    ifstream archivo("../Pruebas/prueba1.txt");
+    string nombre;
+    int peso;
+    archivo >>n >> m >> k;
+    cout << "Datos iniciales:" << n << m << k <<  endl;
     pair<string,int> animales[n];
-    animales[0] = make_pair("cabra", 1); 	
-    cout<<"animal: " << animales[0].first << " grandeza: " << animales[0].second << endl;
-    
+    for(int i = 0;i <= n-1;i+=1){
+        archivo >>nombre >> peso;
+        animales[i] = make_pair(nombre, peso);
+        cout<<"animal: " << animales[i].first << " grandeza: " << animales[i].second << endl;
+    }
     return 1;
 }
 
