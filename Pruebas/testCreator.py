9d5e003bb1a5453e1f736ad38e8dcb05747a598b
Jorgemayor <mayor.jorge@correounivalle.edu.co>
2020-05-21 23:11:11 -0500

    Added validations

--- Pruebas/testCreator.py
+++ Pruebas/testCreator.py
@@ -9,7 +9,7 @@ parser.add_argument('--version', action='version', version='1.0.0')
 parser.add_argument('test', help='Name of the test case to create')
 parser.add_argument('n', help='Number of animals', type=int)
 parser.add_argument('m', help='Number of parts', type=int)
-parser.add_argument('k', help='Number of scenes', type=int)
+parser.add_argument('k', help='Number of scenes in the m-1 last parts', type=int)
 
 args = parser.parse_args()
 
@@ -19,16 +19,30 @@ n = args.n
 m = args.m
 k = args.k
 
-if not 1 <= k * (m - 1) <= n * (n - 1) * (n - 2) / 6:
-	print("""Invalid data. There are not enough possible scenes.
-Keep in mind the relation:
-1 <= k * (m - 1) <= n * (n - 1) * (n - 2) / 6""")
+# Validations
+if 1 > n:
+	print("ERROR: n must be 1 <= n")
 	exit(1)
+
+if 2 > m or m > 60:
+	print("ERROR: m must be 2 <= m <= 60")
+	exit(1)
+
+if 1 > k or k > n:
+	print("ERROR: k must be 1 <= k <= n")
+	exit(1)
+
+if  k * (m - 1) > n * (n - 1) * (n - 2) / 6:
+	print("""Invalid data. There are not enough possible scenes.
+Keep in mind the relation: k * (m - 1) <= n * (n - 1) * (n - 2) / 6""")
+	exit(1)
+
 
 animal_names = []
 animals_awesomeness = [i for i in range(1, n + 1)]
 scenes = set()
 
+# Creates and writes all the animals
 with open(test_name, 'w') as file_:
 	
 	file_.write(str(n) + ' ' + str(m) + ' ' + str(k) + '\n')
@@ -43,33 +57,38 @@ with open(test_name, 'w') as file_:
 	file_.write('\n')
 
 
-
+# Creates all the scenes
 while len(scenes) < ((m - 1) * k):
 
 	scene = set()
 	while len(scene) < 3:
 		scene.add(random.choice(animal_names))
 	scenes.add(frozenset(scene))
-	
 
+# Writes all the animals in parts
 with open(test_name, 'a') as file_:
 
 	for scene in scenes:
 		for animal in scene:
 			file_.write(animal + ' ')
+
 		file_.write('\n')
 
 	file_.write('\n')
 	
 	
-	for i in range(m-1):
+	for i in range(m - 1):
 		part = set()
+
 		while len(part) < k:
 			part.add(random.choice(list(scenes)))
+
 		scenes -= part
+
 		for scene in part:
 			for animal in scene:
 				file_.write(animal + ' ')
+
 			file_.write('\n')
 
 		file_.write('\n')
